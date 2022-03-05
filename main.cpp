ff47ca79cd0f9c960c771f0b45ecf5cf02cd2589
Adrian Toro <adriandesantis26@gmail.com>
2022-03-04 21:09:11 -0400

    Initial commit
    Signed-off-by: Adrian Toro <adriandesantis26@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+/**
+ * This program will perform the following 3 operations:
+ * - Calculates the roots for a given second-degree polynomial.
+ * - Calculates the volume of a sphere for a given radius.
+ * - Calculates the travelled distance of a vehicle assuming
+ *   uniform rectilinear motion.
+*/
+
+/**
+ * Entry point
+ */
+int main(){
+
+    /**
+     * States
+    */
+
+    while(true){
+
+    }
+
+    return 0;
+}
